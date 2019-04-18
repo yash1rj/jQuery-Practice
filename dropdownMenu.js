31394f19478ada5376bb808a7f1922320a17ada1
Yash Raj <yashrj130@gmail.com>
2019-04-18 17:31:28 +0530

    Update dropdownMenu.js

--- dropdownMenu.js
+++ dropdownMenu.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 	
+	// own version of jQuery
+	// yQuery("my-selector").addClass("MYNEWCLASS");
+	// yQuery(".my-selector").addClass("MYNEWCLASS");
+	// yQuery("#site-title").addClass("siteClass");
+	
+	// find method
+	$('#main').find('p').addClass("HAAH");
+	
+	//first and last
+	console.log($(".profile-submenu a").first().text());
+	console.log($(".profile-submenu a").last().text());
+	
 	// prevent default context menu
 	$(document).on('contextmenu', function() {
 		return false;
@@ -9,6 +21,7 @@ $(document).ready(function() {
 		return false;
 	});
 	
+	/*
 	// determining mouse click button
 	$(document).on('mousedown', function(ev) {
 		ev.preventDefault();
@@ -46,6 +59,7 @@ $(document).ready(function() {
 		
 		console.log(ev.which); // returns which mouse button is clicked
 	});
+	*/
 	
 	// prevent default
 	$('[href="https://google.com"]').on('click', function(e) {
@@ -98,4 +112,91 @@ $(document).ready(function() {
 		$('.txt').val(""); // reverting textarea content to placeholder after performing the above functions
 		
 	});
+	
+	
+	//focus in and out
+	$('.txt').focusin(function() {
+		console.log("Focused in on textarea");
+	});
+	
+	$('.txt').focusout(function() {
+		console.log("Loosing focus on textarea");
+	});
+	
+	/*
+	window.hasAt = 0;
+	window.hasDot = 0;
+	
+	$('input[name=email]').on('keyup', function() {
+		
+		if($(this).val().indexOf('@') > -1) {
+			hasAt++;
+		}
+		if($(this).val().indexOf('.') > -1) {
+			hasDot++;
+		}
+	});
+	
+	if((window.hasAt > 1) && (window.hasDot > 1)) {
+		console.log("VALID");
+		$('.status').html("VALID");
+	}
+	else {
+		$('.status').html("INVALID");
+	}
+	*/
+	
+	$('input[name=email]').on('focusout', function() {
+		if($(this).val().indexOf('@') > -1 && $(this).val().indexOf('.') > -1) {
+			$('.status').html("VALID email");
+		}
+		else {
+			$('.status').html("INVALID email");
+		}
+	});
+	
+	//contains selector
+	$('p:contains("Lorem")').html("This had Lorem in it. It still has.");
+	
+	//is selector
+	if($(':contains("Lorem")').is("p")) {
+		console.log("Lorem is in p tag");
+	}
+	
+	//hasClass selector
+	if($(':contains("Lorem")').hasClass('my-selector')) {
+		console.log("Lorem is in my-selector class");
+	}
+	else {
+		console.log("Some other class");
+	}
+	
+	//each method
+	$('p').each(function() {
+		console.log($(this).text());
+	});
+	
+	console.log($('p').text()); // same as above but prints together all the p tag contents
+	
+	//callback
+	function printThis(el) {
+		console.log($(this).text());
+	}
+	
+	$('p').each(printThis);
+	
+	function printThat(el) {
+		console.log($(this).val());
+	}
+	
+	$('input').focusout(printThat);
+	
+	//css
+	$('input').css({
+		background: '#f00',
+		padding: '10px',
+		borderColor: 'black',
+		'border-style': 'dotted'
+	});
+	
 });
